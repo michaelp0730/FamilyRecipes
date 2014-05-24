6939bbab031df9f28890906c37236bed14f1a53e
Michael Pellegrini <pellegm@amazon.com>
2014-05-23 22:40:10 -0700

    Removed controllers

--- public/js/routers/router.js
+++ public/js/routers/router.js
@@ -19,35 +19,113 @@ app.RecipesRouter = Backbone.Router.extend({
     },
 
     initialize: function() {
+        // Instantiate collections
+        breakfastRecipes = new app.Breakfasts(),
+        saladRecipes = new app.Salads(),
+        soupRecipes = new app.Soups(),
+        entreeRecipes = new app.Entrees(),
+        sidesRecipes = new app.Sides(),
+        dessertRecipes = new app.Desserts();
+
+        // Fetch data for each collection
+        breakfastRecipes.fetch();
+        saladRecipes.fetch();
+        soupRecipes.fetch();
+        entreeRecipes.fetch();
+        sidesRecipes.fetch();
+        dessertRecipes.fetch();
+
         Backbone.history.start();
     },
 
     showHome: function() {
-        showHome();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+
+            breakfastView = new app.ShowBreakfastsView({
+                collection: breakfastRecipes,
+                el: '#breakfast-container'
+            }),
+
+            saladsView = new app.ShowSaladsView({
+                collection: saladRecipes,
+                el: '#salads-container'
+            }),
+
+            soupsView = new app.ShowSoupsView({
+                collection: soupRecipes,
+                el: '#soups-container'
+            }),
+
+            entreesView = new app.ShowEntreesView({
+                collection: entreeRecipes,
+                el: '#entrees-container'
+            }),
+
+            sidesView = new app.ShowSidesView({
+                collection: sidesRecipes,
+                el: '#sides-container'
+            }),
+
+            dessertsView = new app.ShowDessertsView({
+                collection: dessertRecipes,
+                el: '#desserts-container'
+            });
     },
 
     showBreakfasts: function() {
-        showBreakfast();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            breakfastView = new app.ShowBreakfastsView({
+                collection: breakfastRecipes,
+                el: '#breakfast-container'
+            });
     },
 
     showSalads: function() {
-        showSalads();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            saladsView = new app.ShowSaladsView({
+                collection: saladRecipes,
+                el: '#salads-container'
+            });
     },
 
     showSoups: function() {
-        showSoups();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            soupsView = new app.ShowSoupsView({
+                collection: soupRecipes,
+                el: '#soups-container'
+            });
     },
 
     showEntrees: function() {
-        showEntrees();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            entreesView = new app.ShowEntreesView({
+                collection: entreeRecipes,
+                el: '#entrees-container'
+            });
     },
 
     showSides: function() {
-        showSides();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            sidesView = new app.ShowSidesView({
+                collection: sidesRecipes,
+                el: '#sides-container'
+            });
     },
 
     showDesserts: function() {
-        showDesserts();
+        var headerView = new app.HomeHeaderView({el: '#main-header-section'}),
+            recipesView = new app.HomeRecipesView({el: '#main-content'}),
+            dessertsView = new app.ShowDessertsView({
+                collection: dessertRecipes,
+                el: '#desserts-container'
+            });
     },
 
     showBreakfastRecipe: function(id) {
@@ -132,7 +210,7 @@ app.RecipesRouter = Backbone.Router.extend({
             headerView = new app.HomeHeaderView({el: '#main-header-section'}),
             recipesView = new app.HomeRecipesView({el: '#main-content'}),
             searchResultsView = new app.SearchResultsView({el: '#search-results'});
-            
+
 
         breakfastRecipes.fetch({
             success: function() {
