1834b6c417282f507f0457af14d4d2b6e259d75e
Michael Pellegrini <mipelleg@microsoft.com>
2019-07-27 20:31:34 -0700

    Bootstraps new react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div class="row">
+          <div class="col-12">
+            <h1>Family <span>Recipes</span></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
