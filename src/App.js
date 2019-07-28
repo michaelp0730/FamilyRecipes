3f9b2ad337379456b019258af441dc03b1601c18
Michael Pellegrini <mipelleg@microsoft.com>
2019-07-28 08:01:29 -0700

    Adds helper to alphabetize recipes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import LinksRow from './components/linksRow';
+import AlphabetizeRecipes from './helpers/alphabetizeRecipes';
 import Breakfast from './recipes/breakfast';
 import Desserts from './recipes/desserts';
 import Entrees from './recipes/entrees';
@@ -10,15 +11,22 @@ import Soups from './recipes/soups';
 
 function App() {
   let date = new Date();
+  const sortedBreakfast = AlphabetizeRecipes(Breakfast);
+  const sortedSalads = AlphabetizeRecipes(Salads);
+  const sortedSoups = AlphabetizeRecipes(Soups);
+  const sortedEntrees = AlphabetizeRecipes(Entrees);
+  const sortedSides = AlphabetizeRecipes(Sides);
+  const sortedDesserts = AlphabetizeRecipes(Desserts);
+
   return (
     <div className="App">
       <h1>Family <span>Recipes</span></h1>
-      <LinksRow title="Breakfast" recipes={ Breakfast } />
-      <LinksRow title="Salads" recipes={ Salads } />
-      <LinksRow title="Soups" recipes={ Soups } />
-      <LinksRow title="Entrees" recipes={ Entrees } />
-      <LinksRow title="Sides" recipes={ Sides } />
-      <LinksRow title="Desserts" recipes={ Desserts } />
+      <LinksRow title="Breakfast" recipes={ sortedBreakfast } />
+      <LinksRow title="Salads" recipes={ sortedSalads } />
+      <LinksRow title="Soups" recipes={ sortedSoups } />
+      <LinksRow title="Entrees" recipes={ sortedEntrees } />
+      <LinksRow title="Sides" recipes={ sortedSides } />
+      <LinksRow title="Desserts" recipes={ sortedDesserts } />
       <footer className="footer-main">
         {'Copyright 2011 - ' + date.getFullYear() + ' PellegriniPage.com | All Rights Reserved'}
       </footer>
