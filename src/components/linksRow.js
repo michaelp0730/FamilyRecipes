b74cc4675998331bc119e5aaa7f5c3f25edc7caa
Michael Pellegrini <mipelleg@microsoft.com>
2019-08-13 20:07:35 -0700

    Removes back to top link

--- src/components/linksRow.js
+++ src/components/linksRow.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BREAKFAST } from '../constants/types';
 
 const LinksRow = (props) => {
     const { recipes } = props;
@@ -9,12 +8,9 @@ const LinksRow = (props) => {
                  <a href={'/' + recipeType + '/' + recipe.slug + '/'}>{recipe.title}</a>
                </li>
     });
-    const backToTopBtn = recipeType !== BREAKFAST ?
-        <a className="back-to-top" href="/">Back to top</a> : null;
 
     return (
         <section id={ 'section-' + recipeType } className="container" aria-label={props.title}>
-            { backToTopBtn }
             <h2 className={'label heading-label ' + recipeType}>{props.title}</h2>
             <ul className="link-list">
                 { recipesList }
