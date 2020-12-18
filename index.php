b3a5cacb3adec8bc5b94eeea6a54417519c60504
Michael Pellegrini <pellegrinimichael@yahoo.com>
2020-12-18 15:14:52 -0800

    Adds search functionality

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     $desserts_json = json_decode($desserts_file, true);
 
     if( isset($_GET['recipes-search']) ) {
-        $search_val = htmlentities($_GET['recipes-search']);
+        $search_val = strtolower(htmlentities($_GET['recipes-search']));
     }
 
     include './includes/head.php';
@@ -42,78 +42,114 @@
         <section id="section-breakfast" class="container group">
             <h2 class="breakfast heading-label label">Breakfast</h2>
             <ul class="link-list">
-                <? foreach ($breakfast_json as $key => $breakfast_value) { ?>
+                <?
+                    foreach ($breakfast_json as $key => $breakfast_value) {
+                        if (!$search_val || strpos(strtolower($breakfast_value['title']), $search_val) !== false) {
+                ?>
                         <li>
                             <a href="./recipes/?slug=<?= $breakfast_value['slug'] ?>&type=<?= $breakfast_value['type'] ?>">
                                 <?= $breakfast_value['title'] ?>
                             </a>
                         </li>
-                <? } ?>
+                <?
+                        }
+                    }
+                ?>
             </ul>
         </section>
 
         <section id="section-salads" class="container group">
             <h2 class="salads heading-label label">Salads</h2>
             <ul class="link-list">
-                <? foreach ($salads_json as $key => $salad_value) { ?>
+                <?
+                    foreach ($salads_json as $key => $salad_value) {
+                        if (!$search_val || strpos(strtolower($salad_value['title']), $search_val) !== false) {
+                ?>
                     <li>
                         <a href="./recipes/?slug=<?= $salad_value['slug'] ?>&type=<?= $salad_value['type'] ?>">
                             <?= $salad_value['title'] ?>
                         </a>
                     </li>
-                <? } ?>
+                <?
+                        }
+                    }
+                ?>
             </ul>
         </section>
 
         <section id="section-soups" class="container group">
             <h2 class="soups heading-label label">Soups</h2>
             <ul class="link-list">
-            <? foreach ($soups_json as $key => $soup_value) { ?>
+            <?
+                foreach ($soups_json as $key => $soup_value) {
+                    if (!$search_val || strpos(strtolower($soup_value['title']), $search_val) !== false) {
+            ?>
                 <li>
                     <a href="./recipes/?slug=<?= $soup_value['slug'] ?>&type=<?= $soup_value['type'] ?>">
                         <?= $soup_value['title'] ?>
                     </a>
                 </li>
-            <? } ?>
+            <?
+                    }
+                }
+            ?>
             </ul>
         </section>
 
         <section id="section-entrees" class="container group">
             <h2 class="entrees heading-label label">Entrees</h2>
             <ul class="link-list">
-                <? foreach ($entrees_json as $key => $entree_value) { ?>
+                <?
+                    foreach ($entrees_json as $key => $entree_value) {
+                        if (!$search_val || strpos(strtolower($entree_value['title']), $search_val) !== false) {
+                ?>
                     <li>
                         <a href="./recipes/?slug=<?= $entree_value['slug'] ?>&type=<?= $entree_value['type'] ?>">
                             <?= $entree_value['title'] ?>
                         </a>
                     </li>
-                <? } ?>
+                <?
+                        }
+                    }
+                ?>
             </ul>
         </section>
 
         <section id="section-sides" class="container group">
             <h2 class="sides heading-label label">Sides</h2>
             <ul class="link-list">
-                <? foreach ($sides_json as $key => $side_value) { ?>
+                <?
+                    foreach ($sides_json as $key => $side_value) {
+                        if (!$search_val || strpos(strtolower($side_value['title']), $search_val) !== false) {
+                ?>
                     <li>
                         <a href="./recipes/?slug=<?= $side_value['slug'] ?>&type=<?= $side_value['type'] ?>">
                             <?= $side_value['title'] ?>
                         </a>
                     </li>
-                <? } ?>
+                <?
+                        }
+                    }
+                ?>
             </ul>
         </section>
 
         <section id="section-desserts" class="container group">
             <h2 class="desserts heading-label label">Desserts</h2>
             <ul class="link-list">
-                <? foreach ($desserts_json as $key => $dessert_value) { ?>
+                <?
+                    foreach ($desserts_json as $key => $dessert_value) {
+                        if (!$search_val || strpos(strtolower($dessert_value['title']), $search_val) !== false) {
+                ?>
                     <li>
                         <a href="./recipes/?slug=<?= $dessert_value['slug'] ?>&type=<?= $dessert_value['type'] ?>">
                             <?= $dessert_value['title'] ?>
                         </a>
                     </li>
-                <? } ?>
+                <?
+                        }
+                    }
+                ?>
             </ul>
         </section>
     </div>
